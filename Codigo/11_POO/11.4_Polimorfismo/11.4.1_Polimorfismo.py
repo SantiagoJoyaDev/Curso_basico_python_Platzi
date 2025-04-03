95951a784f2e18134aaa628b1b7e187dd0ce15be
SantiagoJoyaDev <santiagojoyab@outlook.com>
2025-04-03 11:50:40 -0600

    Actualizacion en Polimorfismo y Encapsulamiento (POO)

--- Codigo/11_POO/11.4_Polimorfismo/11.4.1_Polimorfismo.py
+++ Codigo/11_POO/11.4_Polimorfismo/11.4.1_Polimorfismo.py
@@ -2,3 +2,54 @@ print("----------POLIMORFISMO----------")
 #El polimorfismo es un concepto clave en la programación orientada a objetos (POO) que permite que una misma interfaz o método 
 # pueda tener múltiples comportamientos dependiendo del contexto en el que se use. Esto facilita la reutilización del código y 
 # la flexibilidad en el diseño de software.
+
+print("---------POLIMORFISMO DE INCLUSION (AD-HOC)----------")
+#En este polimorfismo lo que se hace es que se utiliza el mismo metodo pero cuando se ingresan los parametos cambiamos como tal
+#los argumentos de esta manera usamos el mismo metodo pero usamos valores diferentes
+class Calculadora:
+    def suma(self, a, b=0, c=0):
+        return a + b + c
+
+calc = Calculadora()
+print(calc.suma(5, 3))      # 8
+print(calc.suma(5, 3, 2))   # 10
+
+print("---------POLIMORFISMO POR HERENCIA----------")
+#En este polimorfismo lo que se hace es que como tal se definen todas las funciones con el mismo metodo pero cuando voy a retorna
+#como tal el valor cambia por lo tanto si llamo el mismo metodo pero con diferente objeto tendria la rta dependiendo del objeto
+#mas no del metodo
+class Animal:
+    def hacer_sonido(self):
+        return "Sonido genérico"
+
+class Perro(Animal):
+    def hacer_sonido(self):
+        return "Guau Guau"
+
+class Gato(Animal):
+    def hacer_sonido(self):
+        return "Miau"
+
+perro = Perro()
+gato = Gato()
+
+print(perro.hacer_sonido())
+print(gato.hacer_sonido())
+
+print("---------POLIMORFISMO POR DUCK TYPING (TIPADO DINAMICO)----------")
+#En este polimorfismo lo que importa es que aunque se use el mismo metodo dependiendo de la clase se comporta diferente
+class Pato:
+    def hacer_sonido(self):
+        return "Cuac Cuac"
+
+class Persona:
+    def hacer_sonido(self):
+        return "Hola!"
+
+def hacer_hablar(objeto):
+    print(objeto.hacer_sonido())
+
+# No importa el tipo, solo que tenga el método `hacer_sonido`
+hacer_hablar(Pato())    # Cuac Cuac
+hacer_hablar(Persona()) # Hola!
+ 
---
+++ Codigo/11_POO/11.5_Encapsulamiento/11.5_Encapsulamiento.py
@@ -0,0 +1,201 @@
+print("----------ENCAPSULAMIENTO----------")
+#El encapsulamiento es uno de los pilares de la Programación Orientada a Objetos (POO). Su objetivo principal es ocultar 
+# los detalles internos de un objeto y proteger su estado, permitiendo que solo se acceda o modifique a través de métodos específicos.
+
+#¿Para qué sirve el encapsulamiento?
+# Protección de datos → Evita que los atributos sean modificados directamente.
+# Control de acceso → Define qué puede modificarse desde fuera de la clase.
+# Modularidad → Facilita la organización del código.
+# Mantenimiento → Permite hacer cambios internos sin afectar a otras partes del programa.
+
+class MiClase:
+    def __init__(self):
+        self._name = "Santiago Joya Blanco"#Los atributos escritos asi --> son entendidos como privados _name,_color, etc etc......
+        self._id = 1005233962
+        self._city = "Bucaramanga"
+        self.__edad = 23#Este es un atributo muy privado por lo que si quiero acceder a el me va arrojar error y de identifican
+        #debido a que ahora se define con doble "__" es decit __name,__color, etc etc....
+        
+    def _presentacion(self):#Tambien existen atributos privados
+        print(f"Hola me llamo {self._name} mi cedula es {self._id} y vivo en {self._city}")
+        
+    def __edad(self):#Tambien existen atributos muy privados
+        print(f" Y mi edad es {self.__edad} ")
+        
+        
+persona = MiClase()
+print(persona._name,persona._id,persona._city)#Si quiero agregar aqui el __edad me arroja error ya que es un atributo muy privado
+persona._presentacion()
+#persona.__edad() entonces esto me arroja error porque como es privado no se puede acceder al metodo privado
+
+print("\n----------GETTERS Y SETTERS----------")
+#Los getters y setters son métodos que permiten controlar el acceso y la modificación de los atributos de una clase. 
+#Se utilizan principalmente para proteger atributos privados y validar datos antes de asignarlos.
+
+#Encapsulamiento → Protegen los atributos de acceso directo.
+# Validación → Permiten validar datos antes de asignarlos.
+# Modificación segura → Se pueden cambiar las reglas sin afectar el código externo.
+# Control de acceso → Se puede hacer que ciertos atributos sean solo de lectura.
+
+#GETTERS
+class Persona:#Con metodo privados
+    def __init__(self,name,age):
+        self._name = name
+        self._age = age
+    
+    def get_nombre(self):
+        return self._name
+    
+    def get_edad(self):
+        return self._age
+
+persona = Persona("Santiago Joya Blanco",23)
+nombre = persona.get_nombre()
+edad = persona.get_edad()
+print("Aqui accedemos con metodos privados getter")
+print("Hola mi nombre es", nombre ,"y mi edad es", edad ,"anios")
+
+class Persona:#Con metodos muy privados
+    def __init__(self,name,age):
+        self.__name = name
+        self.__age = age
+    
+    def get_nombre(self):
+        return self.__name
+    
+    def get_edad(self):
+        return self.__age
+
+persona = Persona("Santiago Joya Blanco",23)
+nombre = persona.get_nombre()
+edad = persona.get_edad()
+print("Aqui accedemos con metodos muy privados getter")
+print("Hola mi nombre es", nombre ,"y mi edad es", edad ,"anios")
+
+#SETTERS
+class Persona:#Con metodo privados
+    def __init__(self,name,age):
+        self._name = name
+        self._age = age
+    
+    def get_nombre(self):
+        return self._name
+    
+    def set_nombre(self,new_name):
+        self._name = new_name
+    
+    def get_edad(self):
+        return self._age
+
+persona = Persona("Santiago Joya Blanco",23)
+nombre = persona.get_nombre()
+edad = persona.get_edad()
+print("\nAqui accedemos con metodos privados setter")
+print("Hola mi nombre es", nombre ,"y mi edad es", edad ,"anios")
+#SET
+persona.set_nombre("Dawin")
+nombre_nuevo = persona.get_nombre()
+print("Aqui accedemos con metodos privados setter")
+print("Hola mi nombre es", nombre_nuevo ,"y mi edad es", edad ,"anios")
+
+class Persona:#Con metodos muy privados
+    def __init__(self,name,age):
+        self.__name = name
+        self.__age = age
+    
+    def get_nombre(self):
+        return self.__name
+    
+    def set_nombre(self,new_name):
+        self.__name = new_name
+    
+    def get_edad(self):
+        return self.__age
+
+persona = Persona("Santiago Joya Blanco",23)
+nombre = persona.get_nombre()
+edad = persona.get_edad()
+print("\nAqui accedemos con metodos muy privados setter")
+print("Hola mi nombre es", nombre ,"y mi edad es", edad ,"anios")
+#SET
+persona.set_nombre("Wendy")
+nombre_nuevo = persona.get_nombre()
+print("Aqui accedemos con metodos muy privados setter")
+print("Hola mi nombre es", nombre_nuevo ,"y mi edad es", edad ,"anios")
+
+print("\n----------DECORADORES----------")
+#Los decoradores en Python son una forma elegante y poderosa de modificar el comportamiento de funciones o métodos 
+#sin modificar su código fuente. Se utilizan con el símbolo @ antes del nombre de la función.
+
+#Añadir funcionalidades a funciones o métodos sin modificar su código.
+# Reutilización de código, evitando duplicación.
+# Mayor legibilidad y organización en el código.
+# Ejecutar código antes o después de una función sin cambiar su estructura.
+
+"""Basicamente lo que se realiza es que realizamos una funcionalidad antes y despues de del llamado de la funcion es decir
+realizamos mas acciones o funcionalidades en una sola funcion y a eso se le llama decorar la funcion es decir la funcion pricipal
+sigue siendo la misma pero se puede agregar una funcionalidad antes o despues...
+"""
+print("Usando el decorador de manera incorrecta")
+def decorador(funcion):#Como tal esta no es la forma de utilizar decorador
+    def funcion_modificada():
+        print("Antes de llamar a la funcion")
+        funcion()
+        print("Despues de llamar a la funcion")
+    return funcion_modificada
+
+def saludo():
+    print("Hola Santiago como vamos ?")
+    
+saludo_modificado = decorador(saludo)#Llamada del decorador
+saludo_modificado()
+
+#Forma correcta de utilizar el decorador
+print("\nUsando el decorador de manera correcta")
+@decorador
+def saludo():
+    print("Hola Santiago como vamos ?")
+saludo()
+
+print("\n----------DECORADOR PROPERTY----------")
+class Persona:#Con metodo privados
+    def __init__(self,name,age):
+        self._name = name
+        self._age = age
+    
+    @property
+    def nombre(self):#de esta manera ocultamos el nombre verdadero de la varibale _name y no es necesario colocar la variable asi sino
+        #sino colocamos nombre y usamos el property
+        return self._name
+    
+    @nombre.setter
+    def nombre(self,new_name):
+        self._name = new_name
+        
+    @nombre.deleter
+    def nombre(self):
+        del self._name
+    
+    @property
+    def edad(self):
+        return self._age
+    
+
+#GET CON DECORADOR
+persona = Persona("Santiago Joya Blanco",23)
+nombre = persona.nombre#La diferencia es que al final ya no le tengo que colocar () ya que estoy usando el @property
+#es decir ya no lo uso como una funcion si no como una PROPIEDAD
+edad = persona.edad
+print("Aqui accedemos con metodos privados getter")
+print("Hola mi nombre es", nombre ,"y mi edad es", edad ,"anios")
+#SET CON DECORADOR
+nombre = persona.nombre = "Dawin valenzuela"
+print("Aqui accedemos con metodos privados setter")
+print("Hola mi nombre es", nombre ,"y mi edad es", edad ,"anios")
+nombre = persona.nombre = "Antonio joya"
+print("Aqui accedemos con metodos privados setter")
+print("Hola mi nombre es", nombre ,"y mi edad es", edad ,"anios")
+#DEL CON DECORADOR
+# del persona.nombre
+# nombre = persona.nombre#Aqui lo borramos por lo tanto si no quito esa linea me arroja un error debido a que ya se borro el atributo
+# print(nombre)
